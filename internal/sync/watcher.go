e29fdcaa8c003e48e4113899e339aa4eb2a94fc0
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-02-14 22:20:04 -0500

    refactor: added subdirectory monitoring

--- internal/sync/watcher.go
+++ internal/sync/watcher.go
@@ -1,25 +1,54 @@
 package sync
 
 import (
+	"context"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
+	"os"
+	"time"
 )
 
-func watch(filepath string) error {
+func watch(ctx context.Context, filepath string, queue chan<- *SyncOp) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return fmt.Errorf("failed to initialize file watching (func watch): %x", err)
 	}
-	defer watcher.Close()
 
-	go func() error {
+	errs := make(chan error, 4)
+
+	go func() {
+		debounceMap := make(map[string]int64)
 		for {
 			select {
+			case <-ctx.Done():
+				watcher.Close()
+				return
 			case event, ok := <-watcher.Events:
 				if !ok {
-					return fmt.Errorf("error occured with watcher.Events (func watch): %w", err)
+					errs <- fmt.Errorf("error occurred with watcher.Events (func watch): %w", err)
 				}
+
+				path := event.Name
+
+				currentTime := time.Now().UnixMilli()
+
+				if _, exists := debounceMap[path]; !exists {
+					debounceMap[path] = currentTime
+				} else if (currentTime - debounceMap[path]) < 6000 {
+					continue
+				}
+
 				if event.Has(fsnotify.Create) {
+					pathInfo, err := os.Stat(path)
+					if err != nil {
+						if os.IsNotExist(err) {
+							errs <- fmt.Errorf("%s file does not exist", path)
+						}
+						errs <- fmt.Errorf("error occurred with os.Stat (func watch): %w", err)
+					}
+					if pathInfo.IsDir() {
+						watcher.Add(path)
+					}
 				}
 				if event.Has(fsnotify.Write) {
 
@@ -28,19 +57,23 @@ func watch(filepath string) error {
 				}
 				if event.Has(fsnotify.Rename) {
 				}
-				if event.Has(fsnotify.Chmod) {
-				}
 
 			case err, ok := <-watcher.Errors:
 				if ok {
-					return fmt.Errorf("error occurred with watcher.Errors (func watch): %w", err)
+					errs <- fmt.Errorf("error occurred with watcher.Errors (func watch): %w", err)
+					return
 				}
-				return nil
 			}
 		}
 	}()
 
-	watcher.Add(filepath)
+	if addErr := watcher.Add(filepath); addErr != nil {
+		return fmt.Errorf("error occurred with watcher.Add (func watch): %w", addErr)
+	}
 
-	return nil
+	finErr, ok := <-errs
+	if !ok {
+		return nil
+	}
+	return finErr
 }
