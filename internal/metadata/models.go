1825be543b0d44344fd9cb186b5099e88eb1a0e8
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-02-18 12:41:15 -0500

    refactor: add rename and ino members to File struct

--- internal/metadata/models.go
+++ internal/metadata/models.go
@@ -17,14 +17,17 @@ type File struct {
 	LastSyncTime time.Time
 	CreatedAt    time.Time
 	Deleted      bool
+	Renamed      bool
+	Inode        uint64
 }
 
-func FileConstructor(path string, hash string, size int64, modTime time.Time, isDir bool) *File {
+func FileConstructor(path string, hash string, size int64, modTime time.Time, isDir bool, ino uint64) *File {
 	return &File{
 		Path:    path,
 		Hash:    hash,
 		Size:    size,
 		ModTime: modTime,
 		IsDir:   isDir,
+		Inode:   ino,
 	}
 }
